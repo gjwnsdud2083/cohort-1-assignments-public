6e140cb6471d0ca5fa67c55215e2a764635993e2
gjwnsdud2083 <gjwnsdud2083@naver.com>
2025-09-08 21:23:00 +0900

    feat: MiniAMMFactory에서 모든 쌍의 길이를 반환하는 기능 구현 및 새로운 쌍 생성 로직 추가

--- 2/src/MiniAMMFactory.sol
+++ 2/src/MiniAMMFactory.sol
@@ -16,11 +16,20 @@ contract MiniAMMFactory is IMiniAMMFactory {
     
     // implement
     function allPairsLength() external view returns (uint256) {
-        return 0;
+        return allPairs.length;
     }
     
     // implement
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        return address(0);
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0) && token1 != address(0), "Zero address");
+        require(token0 != token1, "Identical addresses");
+        require(getPair[token0][token1] == address(0), "Pair exists");
+        pair = address(new MiniAMM(token0, token1));
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair;
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+        return pair;
     }
 }
