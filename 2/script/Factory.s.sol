e265cc2c3fd02d47c0c94596843a16f0d7b5d4f3
gjwnsdud2083 <gjwnsdud2083@naver.com>
2025-09-11 23:30:43 +0900

    chore: .gitignore 업데이트 및 로그 파일 추가
    - .gitignore에 graph/data 및 logs 디렉토리 추가
    - 로그 파일에 대한 새로운 데이터 추가 및 기존 로그 파일 업데이트
    - 불필요한 데이터 추적 방지를 위한 설정 강화

--- 2/script/Factory.s.sol
+++ 2/script/Factory.s.sol
@@ -18,11 +18,18 @@ contract FactoryScript is Script {
         vm.startBroadcast();
 
         // Step 1: Deploy MiniAMMFactory
+        miniAMMFactory = new MiniAMMFactory();
+        console.log("MiniAMMFactory deployed:", address(miniAMMFactory));
 
         // Step 2: Deploy two MockERC20 tokens
+        token0 = new MockERC20("Token A", "TA");
+        console.log("Token0 (TA) deployed:", address(token0));
+        token1 = new MockERC20("Token B", "TB");
+        console.log("Token1 (TB) deployed:", address(token1));
 
         // Step 3: Create a MiniAMM pair using the factory
-
+        pair = miniAMMFactory.createPair(address(token0), address(token1));
+        console.log("MiniAMM Pair deployed:", pair);
         vm.stopBroadcast();
     }
 }
